7b7fa6f353d07e8b81b33e85a6c22d59fba54e02
BoryMary12 <79740978+BoryMary12@users.noreply.github.com>
2021-09-04 18:23:14 +0900

    9/4

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player;
+
+    private Vector3 mousePos;
+
+    private Vector3 angle = new Vector3(0, 0, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            angle = player.transform.localEulerAngles;
+            mousePos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButton(1))
+        {
+            angle.y -= (Input.mousePosition.x - mousePos.x) * 0.1f;
+            angle.x -= (Input.mousePosition.y - mousePos.y) * 0.1f;
+
+            player.gameObject.transform.localEulerAngles = angle;
+
+            mousePos = Input.mousePosition;
+        }
+    }
+}
