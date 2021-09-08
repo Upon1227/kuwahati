5763ccb5d6db328dc62faa770297f953d8f69ecf
kuwapo <83585233+kuwapo@users.noreply.github.com>
2021-09-09 03:03:18 +0900

    AudioParamsSO追加

---
+++ Assets/ScriptKuwa/AudioParamsSO.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class AudioParamsSO : ScriptableObject
+{
+    [Header("BGM音量:再生前に変更しないと反映されない")]
+    [Range(0, 1)]
+    public float BGMVolume;
+
+    [Header("SEの音量の上限")]
+    [Range(0, 1)]
+    public float SEVolume;
+
+    [Header("BGMの音量")]
+    [Range(0, 1)]
+    public float OPThemeVolume;
+    [Range(0, 1)]
+    public float FieldVolume;
+    [Range(0, 1)]
+    public float DanceVolume;
+    [Range(0, 1)]
+    public float EndVolume;
+
+    
+    [Header("SEの音量")]
+    [Range(0, 1)]
+    public float UhoVolume;
+    [Range(0, 1)]
+    public float UkiVolume;
+    [Range(0, 1)]
+    public float DamageVolume;
+    [Range(0, 1)]
+    public float GameOverVolume;
+    [Range(0, 1)]
+    public float KnockOutVolume;
+    [Range(0, 1)]
+    public float BreakVolume;
+    [Range(0, 1)]
+    public float FeelGoodVolume;
+    [Range(0, 1)]
+    public float BooingVolume;
+    [Range(0, 1)]
+    public float WalkVolume;
+    [Range(0, 1)]
+    public float BikeVolume;
+    [Range(0, 1)]
+    public float BikeRunVolume;
+
+
+    public float GetVolume(SoundManager.BGM bgm)
+    {
+        switch (bgm)
+        {
+            case SoundManager.BGM.OPTheme:
+                return OPThemeVolume * BGMVolume;
+            case SoundManager.BGM.Field:
+                return FieldVolume * BGMVolume;
+            case SoundManager.BGM.Dance:
+                return DanceVolume * BGMVolume;
+            case SoundManager.BGM.End:
+                return EndVolume * BGMVolume;
+
+
+
+        }
+        return 0;
+    }
+
+
+    public float GetVolume(SoundManager.SE se)
+    {
+        switch (se)
+        {
+            case SoundManager.SE.Uho:
+                return UhoVolume * SEVolume;
+            case SoundManager.SE.Uki:
+                return UkiVolume * SEVolume;
+            case SoundManager.SE.Damage:
+                return DamageVolume * SEVolume;
+            case SoundManager.SE.GameOver:
+                return GameOverVolume * SEVolume;
+            case SoundManager.SE.KnockOut:
+                return KnockOutVolume * SEVolume;
+            case SoundManager.SE.Break:
+                return BreakVolume * SEVolume;
+            case SoundManager.SE.FeelGood:
+                return FeelGoodVolume * SEVolume;
+            case SoundManager.SE.Booing:
+                return BooingVolume * SEVolume;
+            case SoundManager.SE.Walk:
+                return WalkVolume * SEVolume;
+            case SoundManager.SE.Bike:
+                return BikeVolume * SEVolume;
+            case SoundManager.SE.BikeRun:
+                return BikeRunVolume * SEVolume;
+
+        }
+        return 0;
+    }
+
+    public const string PATH = "AudioParamsSO";
+    private static AudioParamsSO _entity;
+    public static AudioParamsSO Entity
+    {
+        get
+        {
+            //初アクセス時にロードする
+            if (_entity == null)
+            {
+                _entity = Resources.Load<AudioParamsSO>(PATH);
+
+                //ロード出来なかった場合はエラーログを表示
+                if (_entity == null)
+                {
+                    Debug.LogError(PATH + " not found");
+                }
+            }
+
+            return _entity;
+        }
+    }
+}
---
+++ Assets/ScriptKuwa/SoundManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource bgmAudioSource;
+    public AudioSource seAudioSource;
+    public AudioClip[] bgmClips;
+    public AudioClip[] seClips;
+    BGM currentBgm;
+
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public enum BGM
+    {
+        OPTheme,//
+        Field,
+        Dance,
+        End,
+        
+    }
+
+    public enum SE
+    {
+        Uho,
+        Uki,
+        Damage,
+        GameOver,
+        KnockOut,
+        Break,  //
+        FeelGood,
+        Booing,
+        Walk,
+        Bike,
+        BikeRun,
+
+    }
+
+
+    public void SetBGMVolume(float volume)
+    {
+        AudioParamsSO.Entity.BGMVolume = volume;
+        bgmAudioSource.volume = AudioParamsSO.Entity.GetVolume(currentBgm);
+
+    }
+    public void SetSEVolume(float volume)
+    {
+        seAudioSource.volume = volume;
+        AudioParamsSO.Entity.SEVolume = volume;
+    }
+
+    
+
+    public void StopBGM()
+    {
+        bgmAudioSource.Stop();
+    }
+
+
+
+    public void PlayBGM(BGM bgm)
+    {
+        int index = (int)bgm;
+        currentBgm = bgm;
+        bgmAudioSource.volume = AudioParamsSO.Entity.GetVolume(currentBgm);
+        bgmAudioSource.clip = bgmClips[index];
+        bgmAudioSource.Play();
+        Debug.Log(bgmAudioSource.volume);
+    }
+
+    public void PlaySE(SE se)
+    {
+        int index = (int)se;
+        seAudioSource.volume = AudioParamsSO.Entity.GetVolume(se);
+        seAudioSource.PlayOneShot(seClips[index]);
+    }
+
+
+    public bool IsPlaySE()
+    {
+        return seAudioSource.isPlaying;
+    }
+
+
+}
