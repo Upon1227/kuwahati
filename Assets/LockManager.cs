ac94a61eddcbd4bb79644392285490391508c791
Upon1227 <83859070+Uponnn@users.noreply.github.com>
2021-09-10 17:26:36 +0900

    aa

--- Assets/LockManager.cs
+++ Assets/LockManager.cs
@@ -10,7 +10,7 @@ public class LockManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        isLock = false;
     }
 
     // Update is called once per frame
